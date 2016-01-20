426857abd734e9f8a26e38b9f670577551258a4b
Luis Ybarra <lrybarra@gmail.com>
2016-01-20 16:42:51 -0500

    complete 4.3 challenge

---
+++ week-4/angry-boss.rb
@@ -0,0 +1,10 @@
+# Angry Boss Program
+
+puts "Angry Boss Program"
+puts ""
+
+puts "WHADDAYA WANT?!"
+employee_want = gets.chomp
+
+puts ""
+puts "WHADDAYA MEAN \"" + employee_want.upcase + "\"?!?   YOU'RE FIRED!!"
---
+++ week-4/variables-methods.rb
@@ -0,0 +1,34 @@
+# Full name greeting
+
+puts "Enter Your First Name! "
+
+first_name = gets.chomp
+
+puts "Enter Your Middle Name! "
+
+middle_name = gets.chomp
+
+puts "Enter Your Last Name! "
+
+last_name = gets.chomp
+
+puts 'What\'s crackin ' + first_name + ' ' + middle_name + ' ' + last_name + '!'
+
+# Bigger, better favorite numbers
+
+puts ''
+puts 'Enter Your Favorite Number: '
+
+favorite_number = gets.chomp
+result = favorite_number.to_i + 1
+
+puts result.to_s + ' is a bigger and better number!'
+
+=begin
+How do you define a local variable?
+How do you define a method?
+What is the difference between a local variable and a method?
+How do you run a ruby program from the command line?
+How do you run an RSpec file from the command line?
+What was confusing about this material? What made sense?	
+=end
