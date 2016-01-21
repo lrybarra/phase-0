043ca98ecb9e88e3fb84b5865223200b2d3dfc6a
Luis Ybarra <lrybarra@gmail.com>
2016-01-21 18:05:43 -0500

    second half of 4.5 challenge

---
+++ week-4/calculate-grade/my_solution.rb
@@ -0,0 +1,31 @@
+# Calculate a Grade
+
+# I worked on this challenge [with Luis Ybarra].
+
+
+# Your Solution Below
+
+
+# def get_grade(grade)
+# 	if grade >= 90
+# 		return "A"
+# 	elsif grade >= 80
+# 		return "B"
+# 	elsif grade >= 70
+# 		return "C"
+# 	elsif grade >= 60
+# 		return "D"
+# 	else 
+# 		return "F"
+# 	end
+# end
+
+# def get_grade(grade)
+# 	case grade
+# 		when 90..100 else "A" 
+# 		when 80...90 return "B" 
+# 		when 70...80 return "C" 
+# 		when 60...70 return "D" 	
+# 		when 0...60 return "F" 	
+# 	end
+# end
