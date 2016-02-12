034950a71522315e1a8f7ff06a7587769ee27681
Luis Ybarra <lrybarra@gmail.com>
2016-02-12 17:42:33 -0500

    add recovery function

--- week-7/game.js
+++ week-7/game.js
@@ -3,8 +3,9 @@
 // This is a solo challenge
 
 // Your mission description:
-// Overall mission: Make the Playoffs
-// Goals: Win 3 out of 5 Games
+// Overall mission: Win the Dream Championship
+// Goals: Clinch a Playoff Berth
+//		  Have Fun
 // Characters: Guard, Forward, Backup  
 // Objects: Team, Player
 // Functions: Points, 
@@ -33,6 +34,8 @@ var bg_green = "\x1b[42m";
 var bg_yellow = "\x1b[43m";
 var bg_blue = "\x1b[44m";
 var bg_magenta = "\x1b[45m";
+var bg_white = "\x1b[47m";
+var bg_lightblue = "\x1b[104m";
 // Code Begin
 function team(name, g, f, bck, color){
 	this.name = name; 
@@ -58,6 +61,7 @@ function player(name, pos, j_no, off, def) {
 	this.def = def;
 	this.injured = false;
 	this.pts = 0;
+	this.games = 0;
 };
 // pass in array with off and def levels
 // return array with points (4 elements)
@@ -82,55 +86,97 @@ var playGame = function(team1, team2){
 	//console.log(white, "\b------------------------------------------------");
 	p_pts = pts[0] + pts[1];
  	o_pts = pts[2] + pts[3];
+ 	ovr = "\b";
 	if (p_pts > o_pts) {
 		team1.w += 1;
-		console.log(fg_white,"\b-----", team1.name, "win:", p_pts, "to", o_pts, "-----\n");
+		winner = team1.name;
+		w_pts = p_pts;
+		l_pts = o_pts;
 	}
 	else if (p_pts == o_pts){ // Added after Houston "won" a tie.
 		p_pts += 1;
 		pts[0] += 1;
-		console.log(fg_white,"\b-----", team1.name, "win:", p_pts, "to", o_pts, "IN OVERTIME!-----\n");
+		team1.w += 1;
+		winner = team1.name;
+		w_pts = p_pts;
+		l_pts = o_pts;
+		ovr = "IN OVERTIME!";
 	}
 	else {
+		winner = team2.name
 		team1.l += 1;
-		console.log(fg_white, "\b-----", team2.name, "win:", o_pts, "to", p_pts, "-----\n");
+		w_pts = o_pts;
+		l_pts = p_pts;
 	}
+	console.log(fg_white,"\b-----", winner, "win:", w_pts, "to", l_pts, ovr, "-----\n");
 	team1.games += 1;
+	team1.g.games += 1;
+	team1.f.games += 1;
 	team1.g.pts += pts[0];
 	team1.f.pts += pts[1];
 	team1.pts += p_pts;
 	team1.o_pts += o_pts;
+	if (winner == team1.name) {
+		return true;
+	}
+	else
+		return false;
 };
 
 var statistics = function(team){
-	//console.log(team.color, "###################################################");
 	console.log(team.color, "#####", fg_lightwhite, "After", team.games, "games", team.name, "has a record of", team.w, "-", team.l, team.color, "#####");
-	console.log(fg_lightwhite, team.g.name, "averages", Math.round(team.g.pts / team.games), "ppg &", team.f.name, "averages", Math.round(team.f.pts / team.games), "ppg.");
+	console.log(fg_lightwhite, team.g.j_no, team.g.name, "averages", Math.round(team.g.pts / team.g.games), "ppg &", team.f.j_no, team.f.name, "averages", Math.round(team.f.pts / team.f.games), "ppg.");
 	console.log(" Scoring", Math.round(team.pts / team.games), "points per game & Allowing", Math.round(team.o_pts / team.games), "points per game\n");
-    //console.log(team.color, "###################################################\n");
 };
 
 var injury = function(team){
 	var injuries = ["","twisted ankle", "fractued thumb", "sprained toe"];
 	var injur = Math.floor((Math.random() * 3)+ 1);
-	console.log("\x1b[35m", "++", team.g, "suffered a", injuries[injur], "and will be replaced by", team.bck, "++");
-	team.IR = team.g;
-	team.g = team.bck;
-	team.bck = new player("Null I. Undefined", null, NaN, 0, 0);
-	console.log(" ++ Team has signed journeyman", team.bck.name, "to a temporary contract ++");
+	var who_inj = Math.floor((Math.random() * 5)+ 1);
+	if (who_inj >= 5) {
+		team.g.injured = true;
+		IR = team.g;
+		team.g = team.bck;
+	}
+	else {
+		team.f.injured = true;
+		IR = team.f;
+		team.f = team.bck;
+	}
+	console.log(bg_magenta, fg_lightwhite, "++", IR.name, "suffered a", injuries[injur], "and will be replaced by", team.bck.name, "++", bg_black);
+	team.IR = IR;
+	team.bck = new player("Null I.M. Undefined", null, NaN, 0, 0);
+	console.log(" ++ Team has signed journeyman", team.bck.name, "to a temporary contract ++\n");
 };
 
+var the_return = function(team){
+	console.log(bg_lightblue, fg_lightwhite, "++", IR.name, "has returned from the hospital! ++", bg_black);
+	console.log("++ Team has cut journeyman", team.bck.name, "++\n");
+	team.IR.injured = false;
+	if (team.IR.pos == "Guard"){
+		team.bck = team.g;
+		team.g = team.IR;
+	}
+	else {
+		team.bck = team.f;
+		team.f = team.IR;
+	}
+}
+
+var ejection = function(team){
+	var ejections = ["", "arguing with the referee", "taunting the opposition", "kicking the videographer"];
+}
+
 var line_123456789 = function(){
- 	console.log("Congratulations, you have beaten my baksetball video game");
+ 	console.log(fg_red, "C", fg_lightred, "\bO", fg_lightcyan, "\bN", fg_cyan, "\bG", fg_lightgreen, "\bR", fg_green, "\bA", fg_lightmagenta, "\bT", fg_magenta, "\bU", fg_lightyellow, "\bL", fg_yellow, "\bA", fg_lightwhite, "\bT", fg_white, "\bI", fg_lightblue, "\bO", fg_blue, "\bN", fg_lightred, "\bS", fg_lightgray, "\b!", ", you have beaten my baksetball video game");
  	console.log("Created By: Luis Ybarra");
  	console.log("Created: February 11, 2016\n");
- 	console.log("Starring:");
+ 	console.log("High Score: You!");
  	console.log("Thank you for playing, I hope you enjoyed my game");
 }
 
 // Driver Code (Game Code)
 // Create Players max off - 25, max def - 10
-// Player skills matrix?
 var d_wad = new player("D. Wade", "Guard", 3, 17, 4);
 var c_bos = new player("C. Bosh", "Forward", 1, 11, 1);
 var j_har = new player("J. Harden", "Guard", 13, 16, 1);
@@ -152,16 +198,21 @@ var n_vuc = new player("N. Vučević", "Forward", 9, 15, 2);
 var melo = new player("Melo", "Guard", 7, 45, -10);
 var r_lop = new player("R. Lopez", "Forward", 8, 10, 10);
 var c_oba = new player("C. O'Bannon", "Guard", 4, 4444, 4444);
-var m_zie = new player("Maciej Zieliński", 9, 4444, 4444);
-var a_gie = new player("A. Giedraitis", 25, 4444, 4444);
-var bird = new player("Larry Bird", 33, 597, 597);
-var dr_j = new player("Julius Irving", 6, 597, 597);
-var k_pass = new player("A. Jaswinski", 55, 597, 597);
+var m_zie = new player("Maciej Zieliński", "Forward", 9, 4444, 4444);
+var a_gie = new player("A. Giedraitis", "Guard", 25, 4444, 4444);
+var bird = new player("Larry Bird", "Forward", 33, 597, 0);
+var dr_j = new player("Julius Irving", "Forward", 6, 597, 0);
+var k_pass = new player("A. Jaswinski", "Guard", 55, 597, 0);
 var air = new player("Integer Jordan", "Guard", 23, 25, 10);
 var pip = new player("String Pippen", "Forward", 33, 25, 10);
 var rod = new player("Boolean Rodman", "Forward", 91, 10, 91);
+var b_eic = new player("Brendan Eich", "Guard", 95, 5, 10);
+var l_yba = new player("Luis Ybarra", "Forward", 3, 6, 10);
+var plyf$ = new player("Playoffs", "", 500, 0, 0);
+var rose = new player("D. Rose", "Guard", 1, 22, 10);
+var buckets = new player("Jimmy G. Buckets", "Forward", 22, 25, 10);
+var jo = new player("Joakim Noah", "Forward", 13, 9, 13);
 // Create Teams
-var my_team = new team("Chicago Script-a-Bulls", air, pip, rod, fg_red);
 var heat = new team("Miami JavaHeat", d_wad, c_bos, "", fg_yellow);
 var rckt = new team("Houston JavaRockets", j_har, d_how, "", fg_lightred);
 var mvrk = new team("Dallas Javericks", d_wil, d_now, "", fg_lightcyan);
@@ -173,41 +224,59 @@ var pace = new team("Indiana JavaPacers", m_ell, p_geo, "", fg_yellow);
 var mgic = new team("Orlando JavaMagic", v_ola, n_vuc, "", fg_cyan);
 var knck = new team("New York Scripts", melo, r_lop, "", fg_blue);
 var pole = new team("Śląsk Wrocław", c_oba, m_zie, a_gie, fg_green);
-var great1 = new team("All-Time Greats", bird, dr_j, k_pass, fg_white);
-console.log("*****  JavaScript Basketball League  *****");
-my_team.printRoster();
-playGame(my_team, heat);
-playGame(my_team, rckt);
-playGame(my_team, mvrk);
-playGame(my_team, spur);
-//injury(my_team);
-playGame(my_team, suns);
-playGame(my_team, pstn);
-playGame(my_team, cvlr);
+var JS = new team("Java Scripters", b_eic, l_yba, "", fg_lightgreen);
+var great1 = new team("All-Time Greats (597 wins 0 losses)", bird, dr_j, k_pass, fg_white);
 
+var opponents = [heat, rckt, mvrk, spur, suns, pstn, cvlr, pace, mgic, knck, pole, JS, great1];
+var my_team = new team("Chicago Script-a-Bulls", rose, buckets, jo, fg_red);
+
+console.log("*****  JavaScript Basketball League  *****");
+
+my_team.printRoster();
+
+var team_injury = false;
+for (var sea=0; sea<6; sea++){
+	playGame(my_team, opponents[sea]);
+	if (sea == 5)
+		statistics(my_team)
+	if (team_injury == false){
+		inj = Math.floor((Math.random() * 10)+ 1);
+		if (inj == 4){
+			injury(my_team);
+			team_injury = true;
+		}
+	}
+	else {
+		recovery = Math.floor((Math.random() * 10) + 1);
+		if (recovery > 3) {
+			the_return(my_team);
+			team_injury = false;
+		}
+	}
+}
+
+// Playoff Edition
+// for (var ply=6; ply<opponents.length; ply++){
+// 	gameOn = playGame(my_team, opponents[ply]);
+// 	if (gameOn == false)
+// 		break;
+// }
+
+//injury(my_team);
 // Features to Implement
-// ejection
 // player returns from injury
-// Playofffs
 // Mid-game subsitution
-// Season Summary
-statistics(my_team);	
-if (my_team.w >= 5) {
-	console.log("\x1b[42m","You have clinched a playoff berth", "\x1b[40m");
-	//console.log("\x1b[40m", "*******");
+if (my_team.l == 0) {
+	console.log(bg_white, "Here comes the dream championship. It's just you versus the", opponents[opponents.length-1].name, bg_black);
+	var meBool = playGame(my_team, opponents[opponents.length-1]);
+	meBool == true ? line_123456789() : console.log("We\'re sorry, but you have been eliminated from the playoffs");
+}	
+else if (my_team.w >= 5) {
+	console.log(bg_green,"You have clinched a playoff berth", bg_black);
 }
 else {
-	console.log("\x1b[43m","You had a good season but you did not make the playoffs", "\x1b[40m");
-	//console.log("\x1b[40m", "========");
+	console.log(bg_yellow,"You had a good season but you did not make the playoffs", bg_black);
 }
-// IF bck$ = "playoff" THEN GOTO 5000 ELSE GOTO 20
-// Each Game gets progressively more difficult
-// 8888 PRINT "Here comes the dream championship. It's just you vs. the All-Time Greats"
-// PRINT "Championship Game"
-// PRINT "All-Time Greats Record...... 597-0"
-/*
-9000 PRINT "We're sorry, but you have been eliminated from the playoffs"
-*/
 // Refactored Code
 // Reflection
 //
